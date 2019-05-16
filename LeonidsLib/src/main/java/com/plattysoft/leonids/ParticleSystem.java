88d8157fe8c0351cc86461b3479c60db670ffbfe
nicolas <nicolas@recisio.com>
2019-05-16 11:11:07 +0200

    conf: update to deploy with jitpack

--- LeonidsLib/src/main/java/com/plattysoft/leonids/ParticleSystem.java
+++ LeonidsLib/src/main/java/com/plattysoft/leonids/ParticleSystem.java
@@ -7,7 +7,6 @@ import android.animation.ValueAnimator.AnimatorUpdateListener;
 import android.app.Activity;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
-import android.graphics.Rect;
 import android.graphics.drawable.AnimationDrawable;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
